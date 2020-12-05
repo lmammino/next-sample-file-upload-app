758014a630c3e380acb68e744be88030636c9c28
Luciano <lucianomammino@gmail.com>
2020-12-05 11:04:36 +0000

    First commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+import PropTypes from 'prop-types'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+function MyApp ({ Component, pageProps }) {
   return <Component {...pageProps} />
+}
+
+MyApp.propTypes = {
+  Component: PropTypes.element.isRequired,
+  pageProps: PropTypes.object
 }
 
 export default MyApp
