510f833f2aec1ccd7d8a6385c7d8a1b124e12de9
Luciano <lucianomammino@gmail.com>
2020-12-05 12:41:28 +0000

    First draft of mobile design

--- pages/index.js
+++ pages/index.js
@@ -5,61 +5,64 @@ export default function Home () {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>File manager</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <nav className={styles.nav}>
+          <button className={styles.upload_btn}>UPLOAD</button>
+          <input className={styles.search_input} type="text" placeholder="Search documents..." />
+        </nav>
+        <header className={styles.header}>
+          <h2>6 documents</h2>
+          <span>Total size: 600kb</span>
+        </header>
+        <ul className={styles.files_container}>
+          <li>
+            <h3>Doc1</h3>
+            <footer>
+              <span>100kb</span>
+              <button className={styles.delete_btn}>delete</button>
+            </footer>
+          </li>
+          <li>
+            <h3>Doc2</h3>
+            <footer>
+              <span>100kb</span>
+              <button className={styles.delete_btn}>delete</button>
+            </footer>
+          </li>
+          <li>
+            <h3>Doc3</h3>
+            <footer>
+              <span>100kb</span>
+              <button className={styles.delete_btn}>delete</button>
+            </footer>
+          </li>
+          <li>
+            <h3>Doc4</h3>
+            <footer>
+              <span>100kb</span>
+              <button className={styles.delete_btn}>delete</button>
+            </footer>
+          </li>
+          <li>
+            <h3>Doc5</h3>
+            <footer>
+              <span>100kb</span>
+              <button className={styles.delete_btn}>delete</button>
+            </footer>
+          </li>
+          <li>
+            <h3>Doc6</h3>
+            <footer>
+              <span>100kb</span>
+              <button className={styles.delete_btn}>delete</button>
+            </footer>
+          </li>
+        </ul>
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
